ee6b4c1d9d1455284c5210f6c95f2ac451b4a260
Fanfa Ni <nifanfa@foxmail.com>
2021-04-04 15:28:42 +0800

    hello world

---
+++ SimulatedBattery/Program.cs
@@ -0,0 +1,49 @@
+﻿using System;
+
+namespace SimulatedBattery
+{
+    class Program
+    {
+        private static dynamic SimulatedBatterySystemSystemAction;
+
+        static void Main(string[] args)
+        {
+            try
+            {
+                InitWDTF();
+                EnableBattery();
+                for(; ; ) 
+                {
+                    Console.Write("Set Percentage:");
+                    int i = Convert.ToInt32(Console.ReadLine());
+                    SetBatteryPercentage(i);
+                    Console.WriteLine($"Battery Percentage:{i}");
+                }
+            }
+            catch (Exception E)
+            {
+                Console.WriteLine(E);
+                Console.ReadKey();
+            }
+        }
+
+        private static void InitWDTF()
+        {
+            Type WDTFType = Type.GetTypeFromProgID("WDTF2.WDTF");
+            dynamic WDTF = Activator.CreateInstance(WDTFType);
+            SimulatedBatterySystemSystemAction = WDTF.SystemDepot.ThisSystem.GetInterface("SimulatedBatterySystem");
+        }
+
+        private static void EnableBattery() 
+        {
+            SimulatedBatterySystemSystemAction.DisableRealBatteries();
+            SimulatedBatterySystemSystemAction.EnableSimulatedBattery();
+            //hello
+        }
+
+        private static void SetBatteryPercentage(int Value) 
+        {
+            SimulatedBatterySystemSystemAction.SetSimulatedBatteryChargePercentage(Value);
+        }
+    }
+}
