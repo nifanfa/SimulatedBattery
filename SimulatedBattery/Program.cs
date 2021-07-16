f434c32ce2a1e30aca2058a747f7b7f5a1274dfb
nifanfa <nifanfa@foxmail.com>
2021-07-17 01:46:41 +0800

    Update

--- SimulatedBattery/Program.cs
+++ SimulatedBattery/Program.cs
@@ -1,43 +1,109 @@
-﻿using System;
+﻿using SimulatedBattery.Properties;
+using System;
+using System.Diagnostics;
+using System.IO;
+using System.IO.Compression;
 
 namespace SimulatedBattery
 {
     class Program
     {
+        private static bool IsWDTFInstalled 
+        {
+            get { return Type.GetTypeFromProgID("WDTF2.WDTF") != null; }
+        }
         private static dynamic SimulatedBatterySystemSystemAction;
 
         static void Main(string[] args)
         {
-            try
+            Console.ForegroundColor = ConsoleColor.White;
+
+            if (!IsWDTFInstalled) 
             {
-                InitWDTF();
-                for (; ; )
+                Console.WriteLine("WDTF Is Not Installed");
+
+                SetInstaller(Mode.Remove);
+                SetInstaller(Mode.Create);
+
+                Process process = Process.Start("msiexec", $"/i \"{MSIPath}\" /quiet");
+
+                while (!process.HasExited) 
                 {
-                    string s = Console.ReadLine();
-                    switch (s)
-                    {
-                        case "DC":
-                            SetBatteryToDC();
-                            break;
-                        case "AC":
-                            SetBatteryToAC();
-                            break;
-                        case "Enable":
-                            EnableBattery();
-                            break;
-                        case "Disable":
-                            DisableBattery();
-                            break;
-                        default:
-                            SetBatteryPercentage(Convert.ToInt32(s));
-                            break;
-                    }
+                }
+                if (!IsWDTFInstalled) 
+                {
+                    Console.WriteLine("Faild To Install");
+                    Console.ReadKey();
+                    return;
+                }
+                else
+                {
+                    Console.WriteLine("WDTF Installed");
                 }
             }
-            catch (Exception E)
+
+            InitWDTF();
+
+            Console.ForegroundColor = ConsoleColor.Yellow;
+            Console.WriteLine("Commands:"); 
+            Console.WriteLine("Enable: Enable Simulated Battery"); 
+            Console.WriteLine("Disable: Disable Simulated Battery"); 
+            Console.WriteLine("AC: AC Mode"); 
+            Console.WriteLine("DC: DC Mode");
+            Console.WriteLine("Input Any Number To Set Battery Percentage");
+            Console.ForegroundColor = ConsoleColor.White;
+
+            for (; ; )
             {
-                Console.WriteLine(E);
-                Console.ReadKey();
+                string s = Console.ReadLine().ToUpper();
+                switch (s)
+                {
+                    case "DC":
+                        SetBatteryToDC();
+                        break;
+                    case "AC":
+                        SetBatteryToAC();
+                        break;
+                    case "ENABLE":
+                        EnableBattery();
+                        break;
+                    case "DISABLE":
+                        DisableBattery();
+                        break;
+                    default:
+                        SetBatteryPercentage(Convert.ToInt32(s));
+                        break;
+                }
+            }
+        }
+
+        enum Mode 
+        {
+            Create,
+            Remove
+        }
+
+        private static string MSIPath = Path.Combine(Path.GetTempPath(), "Windows Driver Testing Framework (WDTF) Runtime Libraries-x64_en-us.msi");
+        private static string CabPath = Path.Combine(Path.GetTempPath(), "52909056ae20065680e3c9283d5a4a21.cab");
+
+        private static void SetInstaller(Mode mode) 
+        {
+            switch (mode) 
+            {
+                case Mode.Create:
+                    File.WriteAllBytes(MSIPath, Resources.MSI);
+                    File.WriteAllBytes(CabPath, Resources.CAB);
+                    break;
+                case Mode.Remove:
+                    if (File.Exists(MSIPath))
+                    {
+                        File.Delete(MSIPath);
+                    }
+                    if (File.Exists(CabPath))
+                    {
+                        File.Delete(CabPath);
+                    }
+                    break;
             }
         }
 
